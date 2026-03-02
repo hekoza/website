dd16a19e9efc41be36159d0a361d42fa255579f5
Hannah <hekoza@gmail.com>
2026-03-02 02:38:54 -0800

    Gallery code updated in ArtPage.Astro

--- src/scripts/illpages.js
+++
@@ -1,5 +0,0 @@
-export const pages = [
-    { url: "/illustration/characterart", title: "Character Art" },
-    { url: "/illustration/illustrations", title: "Illustrations" },
-    { url: "/illustration/observationalworks", title: "Observational Works" },
-]
